fa67bfd4efc5365b2bd6ff73ff11f375d1bd58d7
alexpat24 <65975068+alexpat24@users.noreply.github.com>
2020-08-29 23:50:51 +0300

    Create task-2.js
    complete

---
+++ task-2.js
@@ -0,0 +1,8 @@
+const countProps = function (obj) {
+  "use strict";
+  // Write code under this line
+  const cuntProps = Object.keys(obj);
+  return cuntProps.length;
+};
+
+<script type="module-3"></script>;
